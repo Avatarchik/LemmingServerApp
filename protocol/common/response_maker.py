a2660b5f0a6e90bd5f8a52db91df63f9b7db5d1a
Gilyoung Kim <xpdlf1004@gmail.com>
2015-09-05 15:21:55 +0900

    Fix bugs that result is null when error occuered.
    Unreviewed.

--- protocol/common/response_maker.py
+++ protocol/common/response_maker.py
@@ -4,4 +4,4 @@ def success(value = ''):
 	return json.dumps({ 'status': 'ok', 'result': [value] })
 
 def error(error):
-	return json.dumps({ 'status': 'fail', 'errorResult': error })
+	return json.dumps({ 'status': 'fail', 'result': [], 'errorResult': error })
